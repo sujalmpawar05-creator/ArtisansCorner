bfdd2af27115df7ddb82621311c0f6dfa1187932
sujalmpawar05-creator <sujalmpawar05@gmail.com>
2026-05-30 13:02:36 +0530

    changes

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,73 +1,3 @@
-// //JWT verify middleware
-// const jwt = require("jsonwebtoken");
-// const User = require("../models/User");
-
-// const authMiddleware = async (req, res, next) => {
-
-//   try {
-
-//     const token = req.header("Authorization");
-
-//     if (!token) {
-//       return res.status(401).json({
-//         success: false,
-//         message: "No token provided"
-//       });
-//     }
-
-//     // Verify token
-//     const decoded = jwt.verify(token, "secretkey");
-
-//     // Find user
-//     const user = await User.findById(decoded.id);
-
-//     if (!user) {
-//       return res.status(401).json({
-//         success: false,
-//         message: "User not found"
-//       });
-//     }
-
-//     req.user = user;
-
-//     next();
-
-//   } catch (error) {
-
-//     res.status(401).json({
-//       success: false,
-//       message: "Invalid token"
-//     });
-
-//   }
-
-// };
-
-// module.exports = authMiddleware;
-
-// //Seller middleware
-// const sellerMiddleware = (req, res, next) => {
-
-//   if (req.user.role !== "seller") {
-
-//     return res.status(403).json({
-//       success: false,
-//       message: "Seller access only"
-//     });
-
-//   }
-
-//   next();
-
-// };
-
-// module.exports.sellerMiddleware = sellerMiddleware;
-
-// module.exports = {
-//   authMiddleware,
-//   sellerMiddleware
-// };
-
 //JWT verify middleware
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
@@ -79,32 +9,23 @@ const authMiddleware = async (req, res, next) => {
     const token = req.header("Authorization");
 
     if (!token) {
-
       return res.status(401).json({
         success: false,
         message: "No token provided"
       });
-
     }
 
     // Verify token
-
-    const decoded = jwt.verify(
-      token,
-      process.env.JWT_SECRET
-    );
+    const decoded = jwt.verify(token, "secretkey");
 
     // Find user
-
     const user = await User.findById(decoded.id);
 
     if (!user) {
-
       return res.status(401).json({
         success: false,
         message: "User not found"
       });
-
     }
 
     req.user = user;
@@ -122,8 +43,9 @@ const authMiddleware = async (req, res, next) => {
 
 };
 
-//Seller middleware
+module.exports = authMiddleware;
 
+//Seller middleware
 const sellerMiddleware = (req, res, next) => {
 
   if (req.user.role !== "seller") {
@@ -138,6 +60,8 @@ const sellerMiddleware = (req, res, next) => {
   next();
 
 };
+
+module.exports.sellerMiddleware = sellerMiddleware;
 
 module.exports = {
   authMiddleware,
